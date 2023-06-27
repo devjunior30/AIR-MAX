5461f9e40a619e6c4b8f11c355bea1ae99747e49
devjunior30 <edyjunior299@gmail.com>
2023-06-27 15:05:56 -0300

    iniciando a estilização do projeto com nova atualização

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let imagem = document.querySelector("#img-principal")
+let aumentarWidth = document.querySelector("#max")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+
+function troquei_a_cor() {
+    
+}
