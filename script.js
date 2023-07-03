03fb8fceb6f199ec8891ced9f8c12760e5fb4a61
devjunior30 <edyjunior299@gmail.com>
2023-07-03 15:07:49 -0300

    pagina em manutenção

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 
 let imagem = document.querySelector("#img-principal")
 let troca = document.querySelector("#troca-cor")
-let text = document.querySelector("h3")
+let text = document.querySelector("p")
 let button = document.querySelector("button")
 let icon = document.querySelectorAll("i")
 let principal = document.querySelector("#logo-airmax")
+let flutuar = document.getElementsByClassName("elemento")
+let apagar = document.querySelector("body")
 
-console.log(icon)
+function transition() {
+    alert(`Ao clicar nas imagens abaixo aparecera sobre a historia
+    de cada air max obs: a pagina ainda esta em manutenção`)
 
-function trocaImagem(endereco) {
+}
+
+function manutenção() {
+    apagar.style.display = "none"
+    alert("pagina em manutenção")
+   
+}
+
+manutenção()
+
+function trocaImagem(endereco, mudei) {
     imagem.src = endereco
-    text.innerHTML = "Design e Qualidade"
-    
+    text.innerHTML = mudei
 }
 
 function myButton(cor) {
